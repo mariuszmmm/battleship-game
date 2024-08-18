146d9f5326ddd51297d3d70d3f0e1c427d18815a
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-08-18 11:14:19 +0200

    small czhanges
    # Conflicts:
    #	src/config/rootSaga.jsx
    #	src/config/store.jsx
    #	src/features/play/Play/getFleet.jsx
    #	src/features/play/Play/index.jsx
    #	src/features/play/playSlice.jsx

--- src/features/play/playSaga.jsx
+++ src/features/play/playSaga.jsx
@@ -3,7 +3,6 @@ import {selectBoard, setStateNewGame, setBoard} from "./playSlice.jsx";
 import {buildShips} from "./Play/buildShips.jsx"
 
 function* actionHandler() {
-
 	const board = yield select(selectBoard);
 	const newBoard = yield call(buildShips, board);
 	yield put(setBoard(newBoard));
