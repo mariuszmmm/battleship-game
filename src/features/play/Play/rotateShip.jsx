d92e8b54a404c6c544e1d3ba3879ac08f1228faf
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-08-20 17:28:10 +0200

    changed rotate ship

--- src/features/play/Play/rotateShip.jsx
+++ src/features/play/Play/rotateShip.jsx
@@ -6,13 +6,13 @@ export const rotateShip = (ship, step = randomMinMax(1, 4)) => {
 		let x_ = item.x
 		switch (step) {
 			case 1:
-				return item
+				return ({y: x_, x: -y_})
 			case 2:
-				return ({y: x_, x: -y_});
-			case 3:
 				return ({y: -y_, x: -x_});
-			case 4:
+			case 3:
 				return ({y: -x_, x: y_});
+			case 4:
+				return item;
 			default:
 				return item;
 		}
