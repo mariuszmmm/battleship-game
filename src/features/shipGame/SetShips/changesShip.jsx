e3d5b81f8a6939cfacb43997b0780c0e83fbd26e
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-09-05 20:57:05 +0200

    changed moving the ships

---
+++ src/features/shipGame/SetShips/changesShip.jsx
@@ -0,0 +1,56 @@
+import {itemHasNeighbor} from "./itemHasNeighbor.jsx";
+import {rotateShip} from "./rotateShip.jsx";
+
+const toTop = (selectedShip) => (
+	selectedShip.map((item) =>
+		({...item, place: {...item.place, row: item.place.row - 1}, selected: true,})
+	));
+
+const toDown = (selectedShip) => (
+	selectedShip.map((item) =>
+		({...item, place: {...item.place, row: item.place.row + 1}, selected: true,})
+	));
+
+const toLeft = (selectedShip) => (
+	selectedShip.map((item) =>
+		({...item, place: {...item.place, col: item.place.col - 1}, selected: true,})
+	));
+
+const toRight = (selectedShip) => (selectedShip.map((item) =>
+	({...item, place: {...item.place, col: item.place.col + 1}, selected: true,})
+));
+
+const rotate = (selectedShip) => {
+	const coordinateShip = () => {
+		return selectedShip.map((item) => {
+			return {x: item.place.col, y: item.place.row}
+		})
+	};
+	const coordinateRotatedShip = rotateShip(coordinateShip());
+	const rotatedShip = coordinateRotatedShip.map((coordinate) => (
+		{
+			numberOfShip: selectedShip[0].numberOfShip,
+			place: {col: coordinate.x, row: coordinate.y},
+			selected: true,
+		}
+	));
+
+	return itemHasNeighbor(rotatedShip)
+}
+
+export const changesShip = (change, selectedShip) => {
+	switch (change) {
+		case "toTop":
+			return toTop(selectedShip);
+		case "toDown":
+			return toDown(selectedShip);
+		case "toLeft":
+			return toLeft(selectedShip);
+		case "toRight":
+			return toRight(selectedShip);
+		case "rotate":
+			return rotate(selectedShip);
+		default:
+			return [];
+	}
+};
